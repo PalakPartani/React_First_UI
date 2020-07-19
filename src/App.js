f507fa7507626641685c81df6ebca8d763da92fe
PalakPartani <partanipalak16@gmail.com>
2020-07-19 02:49:14 -0700

    ADDED:Styles to the UI

--- src/App.js
+++ src/App.js
@@ -4,33 +4,81 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      // <div className="App">
+      <div>
         <header className="header">
-          <p>Company Name</p>
-          <head-left >
-            <p>FEATURES</p>
-            <p>ENTERPRISE</p>
-            <p>SUPPORT</p>
-            <p>LOGIN</p>
-          </head-left>
+          <p>Company Name &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          {/* <head-left > */}
+            {/* <header className="head-left"> */}
+             FEATURES &nbsp;&nbsp;&nbsp;
+            ENTERPRISE &nbsp;&nbsp;&nbsp;
+            SUPPORT &nbsp;&nbsp;
+            LOGIN</p>
+          {/* </header> */}
+          {/* </head-left> */}
         </header>
-        <h3>Pricing</h3>
+        <div className="headingprice">
+          <h5>Pricing</h5>
+        </div>
+        <div className="heading">
+
+
+          <h2>This is first React UI</h2>
+          <p>Quickly build an effective pricing table for your potential</p>
+          <p> customers with this layout.Its built with default </p><p>Material UI Components with little customization </p>
+        </div>
+
+        <div className="box">
+          <div className="firstbox">
+            <div className="free">
+              <p>Free</p>
+            </div>
+
+            <p><h2>$0 /mo</h2>
+          10 users included</p><p> 2 GB of storage</p><p> Help center access </p>
+            <p>Email support</p>
+            <button class="button-color" type="submit">SIGN UP FOR FREE</button>
+          </div>
+
+          <div className="secondbox">
+            <div className="free">
+              <p>Pro</p>
+            </div>
+
+            <h2>$15 /mo</h2>
+            <p> 20 users included</p><p>10 GB of storage</p>
+            <p>Help center access </p>
+            <p>Email support</p>
+            <button class="button-color" type="submit">GET STARTED</button>
+          </div>
+
+          <div className="thirdbox">
+            <div className="enterprise">
+              <p>Enterprise</p>
+            </div>
+            <h2>$30 /mo</h2>
+            <p> 50 users included</p><p>30 GB of storage</p><p> Help center access</p><p>Phone and Email support</p>
+            <button class="button-color" type="submit">CONTACT US</button>
+          </div>
+        </div>
+
+        {/* <h3>Pricing</h3>
         <h2>This is first React UI</h2>
         <p>Quickly build an effective pricing table for your potential</p>
-        <p> customers with this layout.Its built with default </p><p>Material UI Components with little customization </p>
+        <p> customers with this layout.Its built with default </p><p>Material UI Components with little customization </p> */}
 
         {/* <type-f>
           <p>FREE</p>
           <p>PRO</p>
           <p>ENTERPRISE</p>
         </type-f> */}
-        <free-tab>
+        {/* <free-tab>
           <p>FREE</p>
           <p><h2>$0 /mo</h2>
           10 users included</p><p> 2 GB of storage</p><p> Help center access Email support</p>
           <button type="submit">SIGN UP FOR FREE</button>
-        </free-tab>
-        <pro-tab>
+        </free-tab> */}
+        {/* <pro-tab>
           <p>PRO</p>
           <h2>$15 /mo</h2>
           <p> 20 users included</p><p>10 GB of storage</p>
@@ -42,8 +90,47 @@ class App extends Component {
           <h2>$30 /mo</h2>
           <p> 50 users included</p><p>30 GB of storage</p><p> Help center access</p><p>Phone and Email support</p>
           <button type="submit">CONTACT US</button>
-        </enterprise-tab>
-        <down-comp>
+        </enterprise-tab> */}
+        <div className="down-box">
+          <div className="downfirstbox">
+            <div className="down-name">
+              <h5>Company</h5>
+            </div>
+
+            <p>Team</p>
+            <p>History</p>
+            <p>Contact Us</p>
+            <p>Location</p>
+          </div>
+          <div className="downsecondbox">
+            <div className="down-name">
+              <h5>Features</h5>
+            </div>
+            <p>Cool Stuff</p>
+            <p>Random Feature</p>
+            <p>Team Feature</p>
+            <p>Developer Stuff</p>
+            <p>Another One</p>
+          </div>
+          <div className="downthirdbox">
+            <div className="down-name">
+              <h5>Resources</h5>
+            </div>
+            <p>Resource</p>
+            <p>Resource Name</p>
+            <p>Another Resource</p>
+            <p>Final Resource</p>
+          </div>
+          <div className="fourthbox">
+            <div className="down-name">
+              <h5>Legal</h5>
+            </div>
+            <p>Privcy Policy</p>
+            <p>Terms of Use</p>
+          </div>
+
+        </div>
+        {/* <down-comp>
           <h3>Company</h3>
           <p>Team</p>
           <p>History</p>
@@ -71,10 +158,12 @@ class App extends Component {
           <h3>Legal</h3>
           <p>Privcy Policy</p>
           <p>Terms of Use</p>
-        </down-comp>
+        </down-comp> */}
+        <div className="copy">
+          <p><h6>copyright@ Your Website 2020</h6></p>
 
-        <p><h6>copyright@ Your Website 2020</h6></p>
-      </div>
+        </div>
+      </div >
     );
   }
 }
